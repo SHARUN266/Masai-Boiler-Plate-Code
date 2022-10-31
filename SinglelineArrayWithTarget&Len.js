1bc0422a0ad61cbe8f33e2f3ceb2981cc7f9807a
Sharun <101327895+SHARUN266@users.noreply.github.com>
2022-10-31 10:25:55 +0530

    SinglelineArrayWithTarget&Len.js

---
+++ SinglelineArrayWithTarget&Len.js
@@ -0,0 +1,28 @@
+function runProgram(input) {
+  input = input.trim().split("\n");
+  let [len,k]=input[0].trim().split(" ").map(Number);
+  let arr=input[1].trim().split(" ").map(Number)
+  
+
+
+
+
+
+  
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});
