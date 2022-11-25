f34996a00f75968548c9c3e70553bcab79ea1167
Sharun <101327895+SHARUN266@users.noreply.github.com>
2022-11-25 16:33:09 +0530

    2SinleLineArrayWithLengthAndTc.js

---
+++ 2SinleLineArrayWithLengthAndTc.js
@@ -0,0 +1,29 @@
+function runProgram(input) {
+  input = input.trim().split("\n");
+  let line=1;
+  for(var i=0;i<+input[line++];i++){
+      let len=+input[line++];
+      let arr=input[line++].trim().split(" ").map(Number)
+  }
+
+
+
+
+  
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});
