0c28668e59289ec9fdf7484ecc26bbf4b34f971c
Sharun <101327895+SHARUN266@users.noreply.github.com>
2022-12-24 19:23:05 +0530

    RTK Query

---
+++ RTKQueryBoilerPlate.js
@@ -0,0 +1,60 @@
+// First install react-toolkit
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+const UserAPI = createApi({
+  reducerPath: "UserApis",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://mock15-bhoj.onrender.com/",
+  }),
+  endpoints: (builder) => ({
+    allTicket: builder.query({
+      query: (category) => ({
+        url: `/?category=${category}`,
+        method: "GET",
+      }),
+    }),
+    createTicket: builder.mutation({
+      query: (data) => ({
+        url: "/",
+        body: data,
+        method: "POST",
+      }),
+    }),
+    getUser: builder.mutation({
+      query: (data) => ({
+        url: "/user/login",
+        body: data,
+        method: "POST",
+      }),
+    }),
+    createUser: builder.mutation({
+      query: (data) => ({
+        url: "/user",
+        body: data,
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export default UserAPI;
+export const { useAllTicketQuery, useCreateUserMutation, useGetUserMutation,useCreateTicketMutation } =
+  UserAPI;
+
+
+// This is Store folder
+import {configureStore} from "@reduxjs/toolkit"
+
+import UserAPI from "./UserAPI"
+const store=configureStore({
+    reducer:{
+        [UserAPI.reducerPath]:UserAPI.reducer
+    },
+    middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware().concat(UserAPI.middleware)
+
+
+})
+
+
+export default store
