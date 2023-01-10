616656e1b839c475a3892ad11ab68105646d02dc
Sharun <101327895+SHARUN266@users.noreply.github.com>
2023-01-10 21:31:24 +0530

    Update Boiler.js

--- Boiler.js
+++ Boiler.js
@@ -1,1 +1,27 @@
 
+function runProgram(input) {
+  input = input.trim().split("\n");
+
+
+
+
+  
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});
+
+//Enter code here
